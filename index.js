ffc41baacec4b7166de747b2c8431a31a49c2120
James <kamaujames3705@gmail.com>
2023-04-04 22:05:25 +0300

    Added photo generator from API

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-//fetch('https://api.unsplash.com/photos/random?client_id=AF4Ld6qkMDBIh2RixfCUufgf9TX3SHZggBjPOdk_1YE')
+const accessKey = 'AF4Ld6qkMDBIh2RixfCUufgf9TX3SHZggBjPOdk_1YE';
+const img1 = document.getElementById('img1');
+const form = document.getElementById('form');
+const userInput = document.getElementById('userInput');
+
+// Event listeners for submit button not to reload page
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); 
+  
+  const query = userInput.value; // Extract user input from form
+  
+  fetch(`https://api.unsplash.com/search/photos/?client_id=${accessKey}&query=${query}`)
+    .then(response => response.json())
+    .then(data => {
+      const randomIndex = Math.floor(Math.random() * data.results.length);
+      img1.src = data.results[randomIndex].urls.regular;
+      img1.alt = data.results[randomIndex].alt_description;
+    })
+    .catch(error => console.log(error));
+});
+
+
+        
